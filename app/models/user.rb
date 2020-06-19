4c2d91ddd4880c5fa604d2e85e6f3db49f2444ab
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-06-19 14:58:18 +0900

    ゲストログイン

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+    users.password = SecureRandom.urlsafe_base64
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'   
+  } 
+
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy" 
+    get "signup", :to => "users/registrations#new"
+    get "users/:id", :to => "users/registrations#detail"
+    get "login", :to => "users/sessions#new"
+    get "logout", :to => "users/sessions#destroy"
+    post 'users/guest_sign_in', to: "users/sessions#new_guest"
+  end
+
+  post '/tops/guest_sign_in', to: "tops#new_guest"
 end
